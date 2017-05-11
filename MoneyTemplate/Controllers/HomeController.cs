969154405d195002de4515648bf04e63a33fc298
David <divid5566@gmail.com>
2017-05-11 15:11:49 +0800

    選單200筆資料

--- MoneyTemplate/Controllers/HomeController.cs
+++ MoneyTemplate/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MoneyTemplate.Models;
 
 namespace MoneyTemplate.Controllers
 {
@@ -11,6 +12,22 @@ namespace MoneyTemplate.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+        public ActionResult ListDetail()
+        {
+            var List = new List<note>();
+            for (var i = 0; i < 200; i++)
+            {
+                var ListData = new note
+                {
+                    category = i % 2,
+                    noteday = DateTime.Now.AddDays(i).AddHours(i).AddMinutes(i),
+                    description = "預設備註",
+                    noteprice= 10000
+                };
+                List.Add(ListData);
+            }
+            return View(List);
         }
 
         public ActionResult About()
---
+++ MoneyTemplate/Models/note.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MoneyTemplate.Models
+{
+    public class note
+    {
+        public int category { get; set; }
+        public DateTime noteday { get; set; }
+        public int noteprice { get; set; }
+        public string description { get; set; }
+
+
+    }
+}
